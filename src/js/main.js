69f0eb4c0ab1e4b89135d0bcb26cfb76a49aafee
Franmari Garcia <franmarigarciafuentes@gmail.com>
2025-06-16 00:40:05 -0400

    mejorando boton de filtrado, navbar de dispositivos moviles, creando animations.js

--- src/js/main.js
+++ src/js/main.js
@@ -2,14 +2,7 @@ const form = document.getElementById('contact-form');
 const modal = document.getElementById('modal');
 const modalText = document.getElementById('modal-text');
 const modalClose = document.getElementById('modal-close');
-
-window.addEventListener("load", () => {
-  const preloader = document.getElementById("preloader");
-  preloader.style.opacity = "0";
-  preloader.style.visibility = "hidden";
-  preloader.style.transition = "opacity 0.5s ease";
-});
-
+const filterButtons = document.querySelectorAll('.portfolio-filters button');
 
 form.addEventListener('submit', function (e) {
   e.preventDefault();
@@ -29,14 +22,19 @@ function mostrarModal(texto) {
   modal.style.display = 'flex';
 }
 
-// Cerrar el modal al hacer click en el botón
 modalClose.addEventListener('click', () => {
   modal.style.display = 'none';
 });
 
-// (Opcional) Cerrar al hacer click fuera del modal
 window.addEventListener('click', function (e) {
   if (e.target === modal) {
     modal.style.display = 'none';
   }
 });
+
+filterButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    button.classList.toggle('active');
+  });
+});
+
